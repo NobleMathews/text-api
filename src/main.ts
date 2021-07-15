4f9a6e6fc99ce9377c22e825da5b9d162187dc32
Lint Action <lint-action@samuelmeuli.com>
2021-07-15 05:58:51 +0000

    Fix code style issues with ESLint

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import server from "./server";
-const path = require('path')
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 server.listen(
     Number(process.env.PORT ?? 3000),
     "0.0.0.0",
